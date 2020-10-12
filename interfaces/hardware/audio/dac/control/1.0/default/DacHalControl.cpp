c0be30ccbac809971cd912f950d1aaf6aec4dffe
shapeshifter499 <gero3977@gmail.com>
2020-10-12 12:37:23 -0700

    joan: dac: control: rework for Lineage 16.0
    Lineage 16.0 doesn't have support for audio 5.0
    
    Convert lineage-17.1 branch to lineage-16.0 for Pie blobs

--- interfaces/hardware/audio/dac/control/1.0/default/DacHalControl.cpp
+++ interfaces/hardware/audio/dac/control/1.0/default/DacHalControl.cpp
@@ -50,49 +50,23 @@ static std::vector<KeyValue> digital_filters = {{"Short", "0"},
 
 
 DacHalControl::DacHalControl() {
-    mAudioDevicesFactory_V5_0 = ::android::hardware::audio::V5_0::IDevicesFactory::getService();
+    mAudioDevicesFactory_V4_0 = ::android::hardware::audio::V4_0::IDevicesFactory::getService();
 
-    if(mAudioDevicesFactory_V5_0 == nullptr) {
-        LOG(INFO) << "mAudioDevicesFactory_V5_0 null, trying V4_0";
-        mAudioDevicesFactory_V4_0 = ::android::hardware::audio::V4_0::IDevicesFactory::getService();
+    if(mAudioDevicesFactory_V4_0 == nullptr) {
+        LOG(INFO) << "mAudioDevicesFactory_V4_0 null, trying V2_0";
+        mAudioDevicesFactory_V2_0 = ::android::hardware::audio::V2_0::IDevicesFactory::getService();
 
-        if(mAudioDevicesFactory_V4_0 == nullptr) {
-            LOG(INFO) << "mAudioDevicesFactory_V4_0 null, trying V2_0";
-            mAudioDevicesFactory_V2_0 = ::android::hardware::audio::V2_0::IDevicesFactory::getService();
-
-            if(mAudioDevicesFactory_V2_0 == nullptr) {
-                LOG(INFO) << "mAudioDevicesFactory_V2_0 null, aborting";
-                return;
-            } else {
-                usedVersion = AudioVersion::V2_0;
-            }
+        if(mAudioDevicesFactory_V2_0 == nullptr) {
+            LOG(INFO) << "mAudioDevicesFactory_V2_0 null, aborting";
+            return;
         } else {
-            usedVersion = AudioVersion::V4_0;
+                usedVersion = AudioVersion::V2_0;
         }
-    } else {
-        usedVersion = AudioVersion::V5_0;
-    }
+   } else {
+            usedVersion = AudioVersion::V4_0;
+   }
 
     switch(usedVersion) {
-        case AudioVersion::V5_0: {
-                std::function<void(::android::hardware::audio::V5_0::Result,
-                    const android::sp<::android::hardware::audio::V5_0::IDevice>&)> openDevice_cb_V5_0 = [this](::android::hardware::audio::V5_0::Result result,
-                                                                    const android::sp<::android::hardware::audio::V5_0::IDevice>& device)
-                    {
-                        if(result == ::android::hardware::audio::V5_0::Result::OK) {
-                            this->mAudioDevice_V5_0 = device;
-                        } else {
-                            LOG(INFO) << "Couldnt open primary audio device";
-                        }
-                    };
-                mAudioDevicesFactory_V5_0->openDevice("primary", openDevice_cb_V5_0);
-
-                if(mAudioDevice_V5_0 == nullptr) {
-                    LOG(INFO) << "mAudioDevice_V5_0 null, aborting";
-                    return;
-                }
-            }
-            break;
         case AudioVersion::V4_0: {
                 std::function<void(::android::hardware::audio::V4_0::Result,
                     const android::sp<::android::hardware::audio::V4_0::IDevice>&)> openDevice_cb_V4_0 = [this](::android::hardware::audio::V4_0::Result result,
@@ -247,16 +221,8 @@ Return<bool> DacHalControl::setFeatureValue(HalFeature feature, int32_t value) {
     }
     ::android::hardware::audio::V2_0::Result result_V2_0 = ::android::hardware::audio::V2_0::Result::NOT_SUPPORTED;
     ::android::hardware::audio::V4_0::Result result_V4_0 = ::android::hardware::audio::V4_0::Result::NOT_SUPPORTED;
-    ::android::hardware::audio::V5_0::Result result_V5_0 = ::android::hardware::audio::V5_0::Result::NOT_SUPPORTED;
 
     switch(usedVersion) {
-        case AudioVersion::V5_0: {
-                std::vector<::android::hardware::audio::V5_0::ParameterValue> pv_vec = {{kv.name, kv.value}};
-                hidl_vec<::android::hardware::audio::V5_0::ParameterValue> parameters_V5_0 =
-                        hidl_vec<::android::hardware::audio::V5_0::ParameterValue> {pv_vec};
-                result_V5_0 = mAudioDevice_V5_0->setParameters({}, parameters_V5_0);
-            }
-            break;
         case AudioVersion::V4_0: {
                 std::vector<::android::hardware::audio::V4_0::ParameterValue> pv_vec = {{kv.name, kv.value}};
                 hidl_vec<::android::hardware::audio::V4_0::ParameterValue> parameters_V4_0 =
@@ -275,8 +241,7 @@ Return<bool> DacHalControl::setFeatureValue(HalFeature feature, int32_t value) {
     }
 
     if(result_V2_0 == ::android::hardware::audio::V2_0::Result::OK ||
-       result_V4_0 == ::android::hardware::audio::V4_0::Result::OK ||
-       result_V5_0 == ::android::hardware::audio::V5_0::Result::OK) {
+       result_V4_0 == ::android::hardware::audio::V4_0::Result::OK) {
         if(feature != HalFeature::QuadDAC) {
             property_set(property.c_str(), kv.value.c_str());
         }
--- interfaces/hardware/audio/dac/control/1.0/default/DacHalControl.h
+++ interfaces/hardware/audio/dac/control/1.0/default/DacHalControl.h
@@ -23,8 +23,6 @@
 #include <android/hardware/audio/2.0/IDevicesFactory.h>
 #include <android/hardware/audio/4.0/IDevice.h>
 #include <android/hardware/audio/4.0/IDevicesFactory.h>
-#include <android/hardware/audio/5.0/IDevice.h>
-#include <android/hardware/audio/5.0/IDevicesFactory.h>
 
 #include <unordered_set>
 
@@ -47,7 +45,7 @@ using ::vendor::lge::hardware::audio::dac::control::V1_0::HalFeature;
 using ::vendor::lge::hardware::audio::dac::control::V1_0::FeatureStates;
 using ::vendor::lge::hardware::audio::dac::control::V1_0::FeatureState;
 
-enum AudioVersion { V2_0, V4_0, V5_0 };
+enum AudioVersion { V2_0, V4_0 };
 
 class DacHalControl : public IDacHalControl {
   public:
@@ -69,8 +67,6 @@ class DacHalControl : public IDacHalControl {
      android::sp<::android::hardware::audio::V2_0::IDevicesFactory> mAudioDevicesFactory_V2_0;
      android::sp<::android::hardware::audio::V4_0::IDevice> mAudioDevice_V4_0;
      android::sp<::android::hardware::audio::V4_0::IDevicesFactory> mAudioDevicesFactory_V4_0;
-     android::sp<::android::hardware::audio::V5_0::IDevice> mAudioDevice_V5_0;
-     android::sp<::android::hardware::audio::V5_0::IDevicesFactory> mAudioDevicesFactory_V5_0;
 
      AudioVersion usedVersion;
 };
